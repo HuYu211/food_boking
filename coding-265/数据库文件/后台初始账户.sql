b0c2a0bc044099e35a6cd709f79d9a8a31501047
HuYu211 <645889908@qq.com>
2019-04-15 18:36:42 +0800

    4/15

---
+++ coding-265/数据库文件/后台初始账户.sql
@@ -0,0 +1,5 @@
+请将下面的SQL语句执行，然后默认登录账号是54php.cn 密码是123456
+
+INSERT INTO `user` (`uid`, `nickname`, `mobile`, `email`, `sex`, `avatar`, `login_name`, `login_pwd`, `login_salt`, `status`, `updated_time`, `created_time`)
+VALUES
+	(1, '编程浪子www.54php.cn', '11012345679', 'apanly@163.com', 1, '', '54php.cn', '816440c40b7a9d55ff9eb7b20760862c', 'cF3JfH5FJfQ8B2Ba', 1, '2017-03-15 14:08:48', '2017-03-15 14:08:48');
